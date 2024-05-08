56474b7898811f4ab820f7f41380cc407a26619a
Dhnzz <dhanisyahdila@gmail.com>
2024-05-08 20:07:21 +0800

    Update PeriodeController: change method parameters from Periode $periode to Periode $id, update findOrFail calls
    This diff changes the method parameters of several methods in the PeriodeController from Periode

--- app/Http/Controllers/PeriodeController.php
+++ app/Http/Controllers/PeriodeController.php
@@ -29,45 +29,45 @@ class PeriodeController extends Controller
      */
     public function store(Request $request)
     {
-        $dummy = [
-            'Mei 2024',
-            '2024-05-08',
-            '2024-05-31',
-            1
-        ];
-        Periode::create($dummy);
-        return redirect()->route('periode.index')->with('success', 'Data berhasil ditambahkan!');
+        Periode::create($request->all());
+        return redirect()->route('periode.index')->with('success', 'Data periode berhasil ditambahkan!');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Periode $periode)
+    public function show(Periode $id)
     {
-        //
+        $periode = Periode::findOrFail($id);
+        return view('periode.show', compact('periode'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Periode $periode)
+    public function edit(Periode $id)
     {
-        //
+       $periode = Periode::findOrFail($id);
+       return view('periode.edit', compact('periode'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Periode $periode)
+    public function update(Request $request, Periode $id)
     {
-        //
+        $periode = Periode::findOrFail($id);
+        $priode->update($request->all()) ;
+        return redirect()->route('periode.edit')->with('success', 'Data periode berhasil diubah!');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Periode $periode)
+    public function destroy(Periode $id)
     {
-        //
+        $periode = Periode::findOrFail($id);
+        $periode->delete();
+        return redirect()->route('periode.index')->with('success', 'Data periode berhasil dihapus!');
     }
 }
