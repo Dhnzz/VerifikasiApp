72ed9331c7ba160327d6ac52e16d61896343378c
OnyanPokon <mohamadrafiqd@gmail.com>
2024-05-21 23:26:14 +0800

    fix: updating manajemen berkas form

--- resources/views/admin/superadmin/template/manajemen_berkas.blade.php
+++ resources/views/admin/superadmin/template/manajemen_berkas.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.admin')
 
 @section('main')
-<section class="max-w-screen-lg min-h-screen mx-auto flex justify-center items-center py-44 px-4 lg:px-12 gap-4">
+<section class="max-w-screen-lg min-h-screen mx-auto flex items-center py-44 px-4 lg:px-12 gap-4">
   <div class="w-full p-10 bg-white rounded-xl border border-slate-200 shadow-sm flex flex-col">
     <div class="w-full flex flex-col items-center">
       <p class="font-semibold text-lg">Nama Template</p>
@@ -18,8 +18,9 @@
               <span><i class="fas fa-plus"></i></span>
             </x-button_sm>
           </div>
-          <div id="berkas-container" class="flex flex-col ">
+          <div id="berkas-container" class="flex flex-col">
             <!-- Berkas sections will be added here -->
+
           </div>
         </div>
         <x-button_md color="primary" class="w-full col-span-12" type="submit">
@@ -40,29 +41,29 @@
 
     const berkasDiv = document.createElement('div');
     berkasDiv.className = 'berkas mb-4';
+    berkasDiv.id = `berkas${berkasCount}`;
     berkasDiv.innerHTML = `
-    <div class="p-6 bg-slate-100 rounded-xl flex flex-col gap-y-4">
-      <button class="flex justify-between" onclick="openDetails(this, event)">
-        <p class="font-semibold">Berkas ${berkasCount}</p>
-        <span><i class="fas fa-chevron-down text-sm"></i></span>
-      </button>
-      <div class="detailContainer flex flex-col hidden">
-        <div class="mb-4">
-          <label for="nama_berkas" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">
-            Nama Berkas
-          </label>
-          <input type="text" name="nama_berkas[]" id="nama_berkas${berkasCount}" placeholder="Masukan Nama Berkas"
-            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs" />
-        </div>
-        <div class="mb-4">
-          <label for="template_berkas[]" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">
-            Template Berkas
-          </label>
-          <input type="file" name="template_berkas[]" id="template_berkas${berkasCount}" placeholder="Template Berkas"
-            class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs" />
-        </div>
-      </div>
-    </div>
+            <div class="p-6 bg-slate-100 rounded-xl flex flex-col gap-y-4">
+              <button class="flex justify-between" onclick="openDetails(this, event)">
+                <p class="font-semibold">Berkas</p>
+                <span><i class="fas fa-chevron-down text-sm"></i></span>
+              </button>
+              <div class="detailContainer flex flex-col hidden">
+                <div class="mb-4">
+                  <label for="nama_berkas" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">
+                    Nama Berkas
+                  </label>
+                  <input type="text" name="nama_berkas[]" id="nama_berkas${berkasCount}"
+                    placeholder="Masukan Nama Berkas"
+                    class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs" />
+                </div>
+                <div>
+                  <x-button_sm color="danger" class="" onclick="removeBerkas(${berkasCount}, event)">
+                    <i class="fas fa-trash text-sm"></i> Hapus Berkas
+                  </x-button_sm>
+                </div>
+              </div>
+            </div>
     `;
     berkasContainer.appendChild(berkasDiv);
   }
@@ -71,6 +72,12 @@
     event.preventDefault();
     const detailContainer = button.nextElementSibling;
     detailContainer.classList.toggle('hidden');
+  }
+
+  function removeBerkas(berkasId, event) {
+    event.preventDefault();
+    const berkasDiv = document.getElementById(`berkas${berkasId}`);
+    berkasDiv.remove();
   }
 </script>
 @endsection
