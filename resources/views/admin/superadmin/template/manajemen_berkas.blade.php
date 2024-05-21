d6d647bd5f19efca9d16d305bca3e7ed8056bec2
OnyanPokon <mohamadrafiqd@gmail.com>
2024-05-22 03:26:58 +0800

    feat: adding dynamic inputan

--- resources/views/admin/superadmin/template/manajemen_berkas.blade.php
+++ resources/views/admin/superadmin/template/manajemen_berkas.blade.php
@@ -31,15 +31,40 @@
                     Nama Berkas
                   </label>
                   <input type="text" name="nama_berkas[]" id="nama_berkas_statis" placeholder="Masukan Nama Berkas"
-                    class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs" />
+                    value="Konci"
+                    class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs"
+                    oninput="setModalInputValue(this.value)" />
                 </div>
                 <div>
-                  <x-button_sm color="primary" class="edit-button" id="toggle_modal" onclick="modalOpen()">
+                  <x-button_sm color="primary" class="edit-button" id="toggle_modal" onclick="modalOpen(this)">
                     Aksi
                   </x-button_sm>
                 </div>
               </div>
             </div>
+            <div class="p-6 bg-slate-100 rounded-xl flex flex-col gap-y-4">
+              <button class="flex justify-between" onclick="openDetails(this, event)">
+                <p class="font-semibold">Berkas</p>
+                <span><i class="fas fa-chevron-down text-sm"></i></span>
+              </button>
+              <div class="detailContainer flex flex-col">
+                <div class="mb-4">
+                  <label for="nama_berkas" class="block mb-2 text-xs xl:text-sm text-gray-900 dark:text-white">
+                    Nama Berkas
+                  </label>
+                  <input type="text" name="nama_berkas[]" id="nama_berkas_statis" placeholder="Masukan Nama Berkas"
+                    value="Hasda"
+                    class="block w-full xl:p-4 p-3 text-gray-900 border border-gray-300 rounded-md bg-gray-50 xl:text-sm text-xs"
+                    oninput="setModalInputValue(this.value)" />
+                </div>
+                <div>
+                  <x-button_sm color="primary" class="edit-button" id="toggle_modal" onclick="modalOpen(this)">
+                    Aksi
+                  </x-button_sm>
+                </div>
+              </div>
+            </div>
+  
             <!-- Dynamic berkas section will be added here -->
           </div>
         </div>
@@ -53,7 +78,7 @@
           <div class="w-full inline-flex items-center justify-between">
             <p class="text-lg font-semibold">Berkas 1</p>
             <button id="close-modal" class="px-3 py-1.5 rounded-lg hover:bg-slate-100 text-slate-500"
-              onclick="modalOpen()">
+              onclick="closeModal()">
               <i class="fas fa-times text-lg"></i>
             </button>
           </div>
@@ -140,10 +165,26 @@
     }
   }
 
-  function modalOpen() {
+  function modalOpen(value) {
+    console.log(value)
+    const inputan = value.parentElement.previousElementSibling.children[1].value;
+    console.log(inputan)
     const modal = document.getElementById('modal');
-    modal.classList.toggle('hidden');
+    modal.classList.remove('hidden');
+    modal.classList.add('flex')
+    const inputStatis = document.getElementById('nama_berkas_statis');
+    setModalInputValue(inputan);
   }
 
+  function closeModal() {
+    const modal = document.getElementById('modal')
+    modal.classList.remove('flex');
+    modal.classList.add('hidden');
+  }
+  
+ function setModalInputValue(value) {
+    const inputModal = document.getElementById('nama_berkas_modal');
+    inputModal.value = value;
+  }
 </script>
 @endsection
